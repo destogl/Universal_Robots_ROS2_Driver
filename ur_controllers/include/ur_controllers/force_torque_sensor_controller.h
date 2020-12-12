d229a6ef811b403ef194b35f2ddd01bb88d9194b
Lovro <lovro.ivanov@gmail.com>
2020-12-12 13:22:26 +0000

    Prepare force torque state controller

--- ur_controllers/include/ur_controllers/force_torque_sensor_controller.h
+++ ur_controllers/include/ur_controllers/force_torque_sensor_controller.h
@@ -15,22 +15,36 @@
 
 #include <controller_interface/controller_interface.hpp>
 
-namespace ur_controllers {
+#include <geometry_msgs/msg/wrench_stamped.hpp>
 
-    class ForceTorqueStateController: public controller_interface::ControllerInterface{
+namespace ur_controllers
+{
+class ForceTorqueStateController : public controller_interface::ControllerInterface
+{
+public:
+  ForceTorqueStateController();
 
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-    public:
-        controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-        controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+  controller_interface::return_type update() override;
 
-        controller_interface::return_type update() override;
+  CallbackReturn on_configure(const rclcpp_lifecycle::State& previous_state) override;
 
+  CallbackReturn on_activate(const rclcpp_lifecycle::State& previous_state) override;
 
+  CallbackReturn on_deactivate(const rclcpp_lifecycle::State& previous_state) override;
 
-    };
-} // namespace ur_controllers
+protected:
+  bool init_sensor_data();
+  void init_sensor_state_msg();
 
+  //  we store the name of values per axis - compatible interfaces
+  std::vector<std::string> axis_val_names_;
+  std::shared_ptr<rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::WrenchStamped>> wrench_state_publisher_;
+  geometry_msgs::msg::WrenchStamped wrench_state_msg_;
+};
+}  // namespace ur_controllers
 
-#endif //UR_CONTROLLERS_FORCE_TORQUE_SENSOR_CONTROLLER_H
+#endif  // UR_CONTROLLERS_FORCE_TORQUE_SENSOR_CONTROLLER_H
