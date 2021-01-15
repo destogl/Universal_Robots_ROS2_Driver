e0d37e7dd1a1623205b6e4d4012f4a59073cf2e9
Lovro <lovro.ivanov@gmail.com>
2021-01-15 13:22:53 +0000

    Add servo publisher for testing purposes.

---
+++ ur_ros2_control_demos/src/servo_command_publisher.cpp
@@ -0,0 +1,55 @@
+// ROS
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <std_srvs/srv/trigger.hpp>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  static const rclcpp::Logger LOGGER = rclcpp::get_logger("servo_command_publisher");
+
+  // ROS objects
+  rclcpp::NodeOptions node_options;
+  auto node = std::make_shared<rclcpp::Node>("servo_command_publisher", node_options);
+
+  double param_val = strtod(argv[1], NULL);
+  double test = static_cast<double>(param_val);
+  if (param_val <= 2.0 && param_val >= -2.0)
+  {
+    RCLCPP_INFO_STREAM(LOGGER, "Param value = " << param_val);
+  }
+  else
+    return -1;
+
+  // Call the start service to init and start the servo component
+  rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr servo_start_client =
+      node->create_client<std_srvs::srv::Trigger>("/start_servo");
+  servo_start_client->wait_for_service(1s);
+  servo_start_client->async_send_request(std::make_shared<std_srvs::srv::Trigger::Request>());
+
+  std::this_thread::sleep_for(std::chrono::seconds(2));
+  // Create a publisher for publishing the jog commands
+  auto pub = node->create_publisher<geometry_msgs::msg::TwistStamped>("servo_server/delta_twist_cmds", 10);
+  std::weak_ptr<std::remove_pointer<decltype(pub.get())>::type> captured_pub = pub;
+  std::weak_ptr<std::remove_pointer<decltype(node.get())>::type> captured_node = node;
+  auto callback = [captured_pub, captured_node, test]() -> void {
+    auto pub_ptr = captured_pub.lock();
+    auto node_ptr = captured_node.lock();
+    if (!pub_ptr || !node_ptr)
+    {
+      return;
+    }
+    auto msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
+    msg->header.stamp = node_ptr->now();
+    msg->header.frame_id = "tool0";
+    msg->twist.linear.z = test;
+    pub_ptr->publish(std::move(msg));
+  };
+  auto timer = node->create_wall_timer(2ms, callback);
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
